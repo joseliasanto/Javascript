c1fca18eabcb2b95d3719a3faf3e0756e036eee1
Joselia <joselias952@gmail.com>
2023-09-21 14:31:50 -0300

    Novo repositorio

---
+++ primeiro_js/primeiro.js
@@ -0,0 +1,45 @@
+
+var numero = 10
+var numero = 15
+
+let num1 = 15
+num1 = 10
+function pesadelo(){
+
+
+    var a = 15
+    let b = 20
+}
+
+
+a = 10
+b = 25
+
+var nome = "Joselia Santos"
+var nome2 = 'Joselia'
+
+console.log(typeof(nome))
+
+
+let x = '12'
+let y = Number(x)
+
+console.log(Number.isInteger(y))
+
+x = 12.5984
+console.log(x.toFixed(2))
+var x1 = 1
+var x2 = '1'
+console.log(x1+x2)
+console.log('15' + 2)
+console.log('aula' * 2)
+console.log(0.9 - 0.6)
+
+
+const PI = 3.1415
+
+const area = Math.PI * Math.pow(PI , 2)
+console.log(area)
+
+
+
---
+++ segundo js/Js/script.js
@@ -0,0 +1,2 @@
+ //alert("Você entrou no site certo! Aprenda português")
+ document.getElementById('primeiro').innerHTML = "Este h1 foi alterado  confirmado"
